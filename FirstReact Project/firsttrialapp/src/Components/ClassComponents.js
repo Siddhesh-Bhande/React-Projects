15d5ae8014c1603fda642e1e9cdb9da4632a48c0
Siddesh Bhande <siddeshbhande480@gmail.com>
2021-12-31 01:35:54 +0530

    ComponentsCreated

---
+++ FirstReact Project/firsttrialapp/src/Components/ClassComponents.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//for class components 
+class ClassComp extends React.Component{
+    render(){
+        return <p>This is the Class Component</p>
+    }
+}
+
+export default ClassComp;
---
+++ FirstReact Project/firsttrialapp/src/Components/FunctionalComponents.js
@@ -0,0 +1,10 @@
+//first thing to do
+import React from "react";
+
+//component is functional, so add function at start
+//component should always start with uppercase letter
+function FunctionalComponents(){
+    return (<p>This is from functional Component</p>)
+}
+
+export default FunctionalComponents;
